bd2732e5ddf4c47ee4f36be0fa1f90fff1ef0c33
Florian Binder <f.binder@softsystem.de>
2022-05-14 13:41:33 +0200

    added dummy data

--- src/app/modules/pointtracker/containers/result/result.component.ts
+++ src/app/modules/pointtracker/containers/result/result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from 'src/app/modules/enums';
+import { Resultbox } from '../../interfaces';
 
 @Component({
 	selector: 'pt-result',
@@ -6,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
+
+	resultBoxNarrow: Resultbox = {
+		team: Team.NARROW,
+		points: 5,
+	}
+
+	resultBoxWide: Resultbox = {
+		team: Team.WIDE,
+		points: 3,
+	}
+
+
 	constructor() {}
 
 	ngOnInit(): void {}
