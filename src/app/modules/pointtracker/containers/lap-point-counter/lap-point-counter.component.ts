82af8b3c8cf420fdc693c53b742f889b0db9de89
Florian Binder <f.binder@softsystem.de>
2022-05-20 22:17:27 +0200

    navigate through laps

--- src/app/modules/pointtracker/containers/lap-point-counter/lap-point-counter.component.ts
+++ src/app/modules/pointtracker/containers/lap-point-counter/lap-point-counter.component.ts
@@ -5,6 +5,7 @@ import { faArrowLeft, faArrowRight, faMinus, faPlus } from '@fortawesome/free-so
 import * as fromStore from '../../store';
 import { Team } from 'src/app/modules/enums';
 import { Store } from '@ngrx/store';
+import { Lap } from '../../interfaces';
 
 @Component({
 	selector: 'pt-lap-point-counter',
@@ -19,11 +20,50 @@ export class LapPointCounterComponent implements OnInit {
 	faIconMinus: IconDefinition = faMinus;
 	teamInterface = Team;
 
+	btnPrevDisabled: boolean = false;
+	btnPrevLabel: string = 'Zurück';
+
+	btnNextDisabled: boolean = false;
+	btnNextLabel: string = 'Weiter';
+
 	lap$ = this._store.select(fromStore.getCurrentLap);
+
+	private _currentLap!: Lap;
 
 	constructor(
 		private _store: Store<fromStore.PointtrackerState>
 	) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.lap$.subscribe(lap => {
+			this._currentLap = lap;
+			this.btnPrevDisabled = (lap.lapNumber === 1);
+			this.btnNextDisabled = (lap.lapNumber === 6);
+
+			this.btnPrevLabel = (lap.lapNumber === 1) ? 'Zurück' : 'Durchgang ' + (lap.lapNumber - 1);
+			this.btnNextLabel = (lap.lapNumber === 6) ? 'Weiter' : 'Durchgang ' + (lap.lapNumber + 1);
+		});
+	}
+
+	btnPrevClicked(): void {
+		if (this._currentLap.lapNumber > 1) {
+			this._store.dispatch(fromStore.setLap({
+				lap: {
+					lapNumber: this._currentLap.lapNumber - 1,
+					active: true,
+				}
+			}));
+		}
+	}
+
+	btnNextClicked(): void {
+		if (this._currentLap.lapNumber < 6) {
+			this._store.dispatch(fromStore.setLap({
+				lap: {
+					lapNumber: this._currentLap.lapNumber + 1,
+					active: true,
+				}
+			}));
+		}
+	}
 }
